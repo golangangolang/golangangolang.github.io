58d6e2e70863e77e569b36d2605ed1a04a6c8bfa
golangangolang <90753936+golangangolang@users.noreply.github.com>
2025-04-05 10:47:30 +0900

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        blush: '#fbeaea',
+        rose: '#f7c6c7',
+        champagne: '#f7e7ce',
+        gold: '#d4af37',
+        emerald: '#50c878',
+      },
+    },
+  },
+  plugins: [],
+}
